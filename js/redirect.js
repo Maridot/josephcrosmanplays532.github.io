5c5c979376f24d549fc74230a2245ff5aeaa3ea6
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-08 18:18:28 -0400

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -4,3 +4,26 @@ function goBack() {
 function goNext() {
   window.history.next()
 }
+function redirectTo(url) {
+if (url == 'https://discord.io/goanimate4schools') {
+        window.location = "https://discord.io/goanimate4schools";
+    }
+if (url == 'https://discord.io/vyond') {
+        window.location = "https://discord.io/vyond";
+    }
+if (url == 'https://youtube.com/c/JosephAnimate2021') {
+        window.location = "https://youtube.com/c/JosephAnimate2021";
+    }
+if (url == 'https://action-ouranimate.herokuapp.com/link?redirectTo=https://www.discoveryeducation.com') {
+        window.location = "https://action-ouranimate.herokuapp.com/link?redirectTo=https://www.discoveryeducation.com";
+    }
+if (url == 'https://action-ouranimate.herokuapp.com/link?redirectTo=https://office.com/onenote') {
+        window.location = "https://action-ouranimate.herokuapp.com/link?redirectTo=https://office.com/onenote";
+    }
+if (url == 'https://action-ouranimate.herokuapp.com/link?redirectTo=https://clever.com/in/redirector?url=https%3A%2F%2Fsoraapp.com%2Flibrary%2Fccpsmd&amp;link_type=external&amp;link_source=admin_created&amp;hash=60e280d43eb860e0bb7cf29f8043fdea88561553c7c0ee0f431bba7936529c8b&amp;id=61092f4198e8623f508fd882') {
+        window.location = "https://action-ouranimate.herokuapp.com/link?redirectTo=https://clever.com/in/redirector?url=https%3A%2F%2Fsoraapp.com%2Flibrary%2Fccpsmd&amp;link_type=external&amp;link_source=admin_created&amp;hash=60e280d43eb860e0bb7cf29f8043fdea88561553c7c0ee0f431bba7936529c8b&amp;id=61092f4198e8623f508fd882";
+    }
+if (url == 'https://action-ouranimate.herokuapp.com/link?redirectTo=https://my.mheducation.com/secure/student/urn:com.mheducation.openlearning:enterprise.identity.organization:prod.global:organization:705ae22b-9c1c-49bb-88c3-22b125987fe9/home') {
+        window.location = "https://action-ouranimate.herokuapp.com/link?redirectTo=https://my.mheducation.com/secure/student/urn:com.mheducation.openlearning:enterprise.identity.organization:prod.global:organization:705ae22b-9c1c-49bb-88c3-22b125987fe9/home";
+    }
+}
