92ed2c0ef11171ab87fe48928fbc2e65bc38591e
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-10-23 16:19:12 -0400

    Update emulator&game.js

--- js/download/emulator&game.js
+++ js/download/emulator&game.js
@@ -1,9 +1,13 @@
 function downloadEmulator&Game() {
   // this section stats the download of The Legend Of Zelda Oracle Of Seasons
-  setTimeout('Redirect()', 14000);
-  window.location = "/videogames/	Legend of Zelda, The - Oracle of Seasons (U) [C][!].gbc";
+  setTimeout('downloadLegendOfZeldaOracleOfSeasons()', 14000);
+}
+function downloadLegendOfZeldaOracleOfSeasons() {
+  window.location = "/videogames/Legend of Zelda, The - Oracle of Seasons (U) [C][!].gbc";
   // this section starts the download of Visual Boy Advance.
-  setTimeout('Redirect()', 14000);
+  setTimeout('downloadVisualBoyAdvance()', 24000);
+}
+function downloadVisualBoyAdvance() {
   window.location = "https://github.com/josephcrosmanplays532/Visual-Boy-Advance-1.7.2/archive/main.zip";
   document.getElementById('downloadText').style.display = "none";
 }
